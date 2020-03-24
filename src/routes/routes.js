0b99850121118c6157a27b19b56648e284c97874
Rafael Franco <rafael.apfsantos@outlook.com>
2020-03-24 20:17:09 -0300

    [FEAT] - Routes, resources e controllers.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,25 @@
 const express = require('express');
+
+const OngController = require('../controllers/OngController');
+const IncidentController = require('../controllers/IncidentController');
+const ProfileController = require('../controllers/ProfileController');
+const SessionController = require('../controllers/SessionController');
+
 const routes  = express.Router();
 
-routes.post('/users', (request, response) => {
-    const params = request;
-    console.log("Query", params.query);    
-    console.log("Params", params.request);    
-    console.log("Body", params.body);    
-    return response.json({
-        event: 'OmniStacl 11.0',
-        student: "Rafael Franco"
-    });
-});
+// LOGIN
+routes.post('/sessions', SessionController.create);
+
+// ONGS
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+// INCIDENTS
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+// PROFILE
+routes.get('/profiles', ProfileController.index);
 
 module.exports = routes;
