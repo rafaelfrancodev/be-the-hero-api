307e1c7974d6d60626803516b727350f7cb2c50e
Rafael Franco <rafael.apfsantos@outlook.com>
2020-03-24 09:21:41 -0300

    working params - request and response

--- index.js
+++ index.js
@@ -4,8 +4,32 @@ const express = require('express');
 // init application
 const app = express();
 
+// before all requests, express convert json in object in bode request
+app.use(express.json());
+
+/**  
+ * Route / Resource
+ * https://insomnia.rest/download/ - Test API / or POSTMAN
+ */
+
+ /**
+  * Parameters Types
+  * Query: parameter inner url / ?name=tanana
+  * Route Param: /:id
+  * Request Body: Body Reqeust
+  */
+
+
+/**
+ * HTTP / Methods
+ */
+
 // route get
-app.get('/', (request, response) => {
+app.post('/users', (request, response) => {
+    const params = request;
+    console.log("Query", params.query);    
+    console.log("Params", params.request);    
+    console.log("Body", params.body);    
     return response.json({
         event: 'OmniStacl 11.0'
     });
