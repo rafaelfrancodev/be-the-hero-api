3b7f1628f23ab325e7bab99c97185813a085c0dd
Rafael Franco <rafael.apfsantos@outlook.com>
2020-03-25 14:27:19 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // receive library express
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes/routes');
 
 // init application
 const app = express();
 
 // before all requests, express convert json in object in bode request
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
