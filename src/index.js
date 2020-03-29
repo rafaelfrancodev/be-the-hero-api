01d04f4f79c3b3b5b8771d7de4730d3461731f8d
Rafael Franco <rafael.apfsantos@outlook.com>
2020-03-29 13:59:14 -0300

    add validations params, query, body and headers using celebreta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // receive library express
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes/routes');
 
 // init application
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 // listen application in port 3333
 app.listen(3333);
