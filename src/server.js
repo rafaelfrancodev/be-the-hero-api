e8907c16e829f17c41c6921175ed55caba4654f1
Rafael Franco <rafael.apfsantos@outlook.com>
2020-03-30 10:38:23 -0300

    port server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,5 @@
 const app = require('./app');
-const configuration = require('../knexfile');
 
- if (process.env.NODE_ENV === configuration.production) {
-    app.listen(80);
-} else {
-    app.listen(3333);
-}
+app.listen(3333);
+
 
